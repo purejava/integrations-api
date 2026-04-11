0be54ece43ccbfa03672bf222da95d674a5c293f
Ralph Plawetzki <ralph@purejava.org>
2026-04-11 16:32:40 +0200

    Add interface WindowsIcoPath

--- src/main/java/org/cryptomator/integrations/tray/TrayIconLoader.java
+++ src/main/java/org/cryptomator/integrations/tray/TrayIconLoader.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.ApiStatus;
  * A callback used by the {@link TrayMenuController} to load tray icons in the format required by the implementation.
  */
 @ApiStatus.Experimental
-sealed public interface TrayIconLoader permits TrayIconLoader.PngData, TrayIconLoader.FreedesktopIconName {
+sealed public interface TrayIconLoader permits TrayIconLoader.PngData, TrayIconLoader.FreedesktopIconName, TrayIconLoader.WindowsIcoPath {
 
 	@FunctionalInterface
 	non-sealed interface PngData extends TrayIconLoader {
@@ -28,5 +28,16 @@ sealed public interface TrayIconLoader permits TrayIconLoader.PngData, TrayIconL
 		 * @param iconName the icon name
 		 */
 		void lookupByName(String iconName);
+	}
+
+	@FunctionalInterface
+	non-sealed interface WindowsIcoPath extends TrayIconLoader {
+
+		/**
+		 * Loads an icon from a Windows .ico file path.
+		 *
+		 * @param path absolute path to the .ico file
+		 */
+		void loadIcoPath(String path);
 	}
 }
